0f20f0e2391afacb14a0d6175a04ec3f8164933d
brianstorm14 <jafeth1114@gmail.com>
2025-01-17 18:58:14 -0600

    Correción de poner múltiples inputs juntos y quitar botón de levantar

--- app.py
+++ app.py
@@ -8,31 +8,25 @@ st.header("Interfaz de control de desarrollos.")
 
 if "num_desarrollos" not in st.session_state:
     st.session_state.num_desarrollos = 0
-    st.session_state.desarrollos = [] 
+    st.session_state.desarrollos = []
     st.session_state.puertos = []
 
+nombre_input = st.text_input("Nombre del Desarrollo", "")
+puerto_input = st.text_input("Puerto del Desarrollo", "")
+
 if st.button("Agregar"):
-    st.session_state.num_desarrollos += 1
-    st.session_state.desarrollos.append("")
-    st.session_state.puertos.append(None)
+    if nombre_input:
+        st.session_state.desarrollos.append(nombre_input)
+        if puerto_input.isdigit():
+            st.session_state.puertos.append(int(puerto_input))
+        else:
+            st.session_state.puertos.append(8501 + st.session_state.num_desarrollos)
+        st.session_state.num_desarrollos += 1
 
-for i in range(st.session_state.num_desarrollos):
-    st_cols_inputs = st.columns(2)
-
-    with st_cols_inputs[0]:
-        st.session_state.desarrollos[i] = st.text_input(
-            f"Nombre del Desarrollo {i + 1}",
-            value=st.session_state.desarrollos[i],
-            key=f"nombre_{i}"
-        )
-
-    with st_cols_inputs[1]:
-        puerto = st.text_input(
-            f"Puerto del Desarrollo {i + 1}",
-            value=st.session_state.puertos[i] or f"{8501 + i}",
-            key=f"puerto_{i}"
-        )
-        st.session_state.puertos[i] = int(puerto) if puerto.isdigit() else 8501 + i
+        st.write(f"Ejecutando app para Desarrollo {nombre_input}")
+        subprocess.run([f"{sys.executable}", f"app{st.session_state.num_desarrollos}.py"])
+    else:
+        st.warning("Por favor, ingresa un nombre para el desarrollo.")
 
 for i in range(st.session_state.num_desarrollos):
     st_cols_header = st.columns(3)
@@ -43,16 +37,10 @@ for i in range(st.session_state.num_desarrollos):
     with st_cols_header[1]:
         estado = "En proceso"
         color = "#32CD32"
-        st.markdown(f"<p font-weight:bold;'>{estado}</p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='color:{color}; font-weight:bold;'>{estado}</p>", unsafe_allow_html=True)
         st.write("Puerto:", st.session_state.puertos[i])
 
     with st_cols_header[2]:
-        Levantar = st.button(f"Levantar {i + 1}", key=f"Levantar_{i}", use_container_width=True)
         Tirar = st.button(f"Tirar {i + 1}", key=f"Tirar_{i}", use_container_width=True)
-
-        if Levantar:
-            st.write(f"Ejecutando app para Desarrollo {st.session_state.desarrollos[i]}")
-            subprocess.run([f"{sys.executable}", f"app{i+1}.py"])
-
         if Tirar:
             st.write(f"Parando app para Desarrollo {st.session_state.desarrollos[i]}")
