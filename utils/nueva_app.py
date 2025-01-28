14f0805b7b5e94d38ef011dd1a1f234d9fbe197e
brianstorm14 <jafeth1114@gmail.com>
2025-01-28 10:32:41 -0600

    Adición de ejecución del script de consola en Windows

--- utils/nueva_app.py
+++ utils/nueva_app.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import json
+import platform
 import streamlit as st
 
 from utils.config_input import parsear_config
@@ -8,55 +9,70 @@ from utils.config_input import configuracion
 
 def add_app(name, puerto, config, pid_file):
     ruta_proyecto, nombre_script = os.path.split(name)
-    script_sh = f"{nombre_script}.sh"
+    sistema_operativo = platform.system()
 
-    if os.path.exists(os.path.join(ruta_proyecto, nombre_script)):
-        try:
-            config_dict = parsear_config(config)
 
-            config_args = []
-            for key, value in config_dict.items():
-                config_args.extend(configuracion(key, value))
-            
-            # ruta_entorno = os.path.join(ruta_proyecto, "venv")
+    if not os.path.exists(os.path.join(ruta_proyecto, nombre_script)):
+        st.error(f"El script {nombre_script} no fue encontrado en la ruta_proyecto {ruta_proyecto}.")
+        return
 
-            with open(script_sh, "w") as sh_file:
-                sh_file.write(f"#!/bin/bash\n")
-                sh_file.write(f"cd {ruta_proyecto}\n")
-                sh_file.write(f"source venv/bin/activate\n")
-                sh_file.write(f"streamlit run {nombre_script} --server.port {puerto} {' '.join(config_args)}\n")
-            
-            os.chmod(script_sh, 0o775)
+    try:
+        config_dict = parsear_config(config)
+        config_args = []
+        for key, value in config_dict.items():
+            config_args.extend(configuracion(key, value))
 
-            process = subprocess.Popen(
-                ["bash", script_sh],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                preexec_fn=os.setsid,
-            )
+        if sistema_operativo == "Windows":
+            script_file = f"{nombre_script}.bat"
+            script_content = f"""
+            @echo off
+            cd {ruta_proyecto}
+            call venv\\Scripts\\activate
+            streamlit run {nombre_script} --server.port {puerto} {' '.join(config_args)}
+            """
+        else:
+            script_file = f"{nombre_script}.sh"
+            script_content = f"""
+            #!/bin/bash
+            cd {ruta_proyecto}
+            source venv/bin/activate
+            streamlit run {nombre_script} --server.port {puerto} {' '.join(config_args)}
+            """
 
-            data = {}
-            if os.path.exists(pid_file):
-                with open(pid_file, "r") as f:
-                    try:
-                        data = json.load(f)
-                    except json.JSONDecodeError:
-                        st.warning(f"El archivo {pid_file} está vacío o es incorrecto.")
-                        data = {}
+        with open(script_file, "w") as f:
+            f.write(script_content)
 
-            data[nombre_script] = {
-                "pid": process.pid,
-                "puerto": puerto,
-                "nombre": nombre_script,
-                "ruta_proyecto": ruta_proyecto,
-                "config": config_dict,
-            }
+        if sistema_operativo != "Windows":
+            os.chmod(script_file, 0o755)
 
-            with open(pid_file, "w") as f:
-                json.dump(data, f, indent=4)
+        process = subprocess.Popen(
+            script_file if sistema_operativo == "Windows" else ["bash", script_file],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True if sistema_operativo == "Windows" else None,
+            preexec_fn=os.setsid if sistema_operativo != "Windows" else None,
+        )
 
-            st.success(f"El desarrollo {nombre_script} fue levantado con éxito.")
-        except Exception as e:
-            st.error(f"Error al intentar levantar el script: {e}")
-    else:
-        st.error(f"El script {nombre_script} no fue encontrado en la ruta especificada.")
+        data = {}
+        if os.path.exists(pid_file):
+            with open(pid_file, "r") as f:
+                try:
+                    data = json.load(f)
+                except json.JSONDecodeError:
+                    st.warning(f"El archivo {pid_file} está vacío o es incorrecto.")
+                    data = {}
+
+        data[nombre_script] = {
+            "pid": process.pid,
+            "puerto": puerto,
+            "nombre": nombre_script,
+            **config_dict
+        }
+
+        with open(pid_file, "w") as f:
+            json.dump(data, f, indent=4)
+
+        st.success(f"Desarrollo {nombre_script} iniciado en el puerto {puerto}.")
+
+    except Exception as e:
+        st.error(f"Error al intentar levantar el script: {e}")
